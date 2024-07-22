c021254f5b3942b16f05bf133890708c4203a9a6
scarol-simao <carolineduarda13@gmail.com>
2024-07-22 18:12:06 -0300

    semana 3 finalizada

---
+++ Comex Oficial/Comex Oficial/Funcionalidades/GerenciaEstoque.cs
@@ -0,0 +1,81 @@
+﻿using Comex.Funcionalidades;
+using Comex.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comex_Oficial.Funcionalidades
+{
+    internal class GerenciaEstoque : Funcionalidade
+    {
+        private Dictionary<string, Produtos> produtosCadastrados;
+        private int[] estoque;
+
+        public GerenciaEstoque(Dictionary<string, Produtos> produtosCadastrados, int[] estoque)
+        {
+            this.produtosCadastrados = produtosCadastrados;
+            this.estoque = estoque;
+        }
+
+        public override async Task Executa(Dictionary<int, Funcionalidade> opcoesDoDicionario)
+        {
+            await AtualizaEstoque();
+        }
+
+        private async Task AtualizaEstoque()
+        {
+            Console.WriteLine("Digite (1) para adicionar itens ao estoque.");
+            Console.WriteLine("Digite (2) para remover itens do estoque.");
+            string opcao = Console.ReadLine();
+
+            Console.Write("Digite o nome do produto: ");
+            string nomeProduto = Console.ReadLine();
+
+            if (!produtosCadastrados.ContainsKey(nomeProduto))
+            {
+                Console.WriteLine("Produto não cadastrado.");
+                await Pausar();
+                return;
+            }
+
+            Produtos produto = produtosCadastrados[nomeProduto];
+            int indiceProduto = new List<string>(produtosCadastrados.Keys).IndexOf(nomeProduto);
+
+            Console.Write("Digite a quantidade: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            if (opcao == "1")
+            {
+                estoque[indiceProduto] += quantidade;
+                Console.WriteLine($"Produto {nomeProduto} atualizado. Nova quantidade em estoque: {estoque[indiceProduto]}");
+            }
+            else if (opcao == "2")
+            {
+                if (estoque[indiceProduto] >= quantidade)
+                {
+                    estoque[indiceProduto] -= quantidade;
+                    Console.WriteLine($"Produto {nomeProduto} atualizado. Nova quantidade em estoque: {estoque[indiceProduto]}");
+                }
+                else
+                {
+                    Console.WriteLine("Quantidade insuficiente em estoque.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Opção inválida.");
+            }
+
+            await Pausar();
+        }
+
+        private async Task Pausar()
+        {
+            Console.WriteLine("\n\nPressione qualquer tecla para voltar ao menu...");
+            Console.ReadKey(true);
+            Console.Clear();
+        }
+    }
+}
--- Comex Oficial/Comex Oficial/Program.cs
+++ Comex Oficial/Comex Oficial/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Comex.Funcionalidades;
 using Comex.Modelos;
+using Comex_Oficial.Funcionalidades;
 
 namespace Comex.Program
 {
@@ -12,15 +13,23 @@ namespace Comex.Program
         public static Dictionary<string, Cliente> clientesCadastrados = new Dictionary<string, Cliente>();
         public static List<Categoria> categoriasCadastradas = new List<Categoria>();
         public static Dictionary<string, Produtos> produtosCadastrados = new Dictionary<string, Produtos>();
+        public static int[] estoque;  
+        public static Dictionary<string, int> carrinho = new Dictionary<string, int>();  
 
         static async Task Main(string[] args)
         {
+            
+            estoque = new int[produtosCadastrados.Count];
+
             opcoesDoDicionario.Add(0, new Sair());
             opcoesDoDicionario.Add(1, new CadastraCliente(clientesCadastrados));
             opcoesDoDicionario.Add(2, new ListaClientes(clientesCadastrados));
             opcoesDoDicionario.Add(3, new CadastraCategoria(categoriasCadastradas));
             opcoesDoDicionario.Add(4, new ListaCategorias(categoriasCadastradas));
             opcoesDoDicionario.Add(5, new ListaProdutos());
+            opcoesDoDicionario.Add(6, new GerenciaEstoque(produtosCadastrados, estoque));
+            opcoesDoDicionario.Add(7, new AdicionaProdutoAoCarrinho(produtosCadastrados, estoque, carrinho)); 
+
             await ExibirMenuDeOpcoes();
             Console.ReadLine();
         }
@@ -44,6 +53,8 @@ namespace Comex.Program
             Console.WriteLine("Digite (3) para Cadastrar uma Categoria:  ");
             Console.WriteLine("Digite (4) para ver a Lista de Categorias:  ");
             Console.WriteLine("Digite (5) para ver a Lista de Produtos por Categorias:  ");
+            Console.WriteLine("Digite (6) para Gerenciar o Estoque:  ");
+            Console.WriteLine("Digite (7) para Adicionar Produtos ao Carrinho:  ");  
             Console.WriteLine("Digite (0) para sair:  ");
             Console.Write("\nDigite a sua opção:  ");
             string opcaoEscolhida = Console.ReadLine();
